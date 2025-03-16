a698c8d9d5b77de818dbe6793dcd0831e1abceef
Luis LT <105554066+LTprograms@users.noreply.github.com>
2025-03-16 17:51:26 -0500

    test con api real funciona

--- frontend/src/js/api.js
+++ frontend/src/js/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const SERVER_URL = "http://127.0.0.1:5000";
+// const SERVER_URL = "http://127.0.0.1:5000";
+const SERVER_URL = "https://test-frontend-dev.onrender.com";
 
 export function loginUser(username, password) {
     return axios.post(SERVER_URL + "/login", 
